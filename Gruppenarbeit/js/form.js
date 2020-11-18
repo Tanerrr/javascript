c4b7715bb59cb01cd7f78d691d14fb73501d1e5f
Taner Öztaş <taner@oztas.net>
2020-11-18 23:15:19 +0100

    form.js funktioniert

--- Gruppenarbeit/js/form.js
+++ Gruppenarbeit/js/form.js
@@ -9,3 +9,52 @@
  * https://github.com/axios/axios
  * 
  */
+
+const inputfullname = document.getElementById('input-fullname');
+const number = document.getElementById('input-number');
+const email = document.getElementById('input-email');
+const textboxform = document.getElementById('input-message');
+const errorElement = document.getElementById('error');
+const checkbox = document.getElementById('input-privacy');
+
+document.getElementById('submit').addEventListener('click', function submitForm(event){
+    let messages = []
+
+    if (checkbox.checked === false) {
+         messages.push('Checkbox must be checked')
+     }
+
+    if (inputfullname.value === "" || inputfullname.value == null) {
+        messages.push('Name is required');
+        inputfullname.className += " border-red-500";  
+    }else{
+        inputfullname.className += " border-blue-500";
+    }
+    if (number.value === "" || number.value == null) {
+        messages.push('Number is required');
+        number.className += " border-red-500";
+    }
+    if (email.value === "" || email.value == null) {
+        messages.push('Email field cannot be blank');
+        email.className += " border-red-500";
+    }
+    if (textboxform.value === "" || textboxform.value == null) {
+        messages.push('Message is required');
+        textboxform.className += " border-red-500";
+    }
+    if (messages.length > 0) {
+        event.preventDefault();
+        errorElement.innerText = messages[0] //.join(', ')       
+    }
+
+    const formData = new FormData(this);
+
+    const request = new XMLHttpRequest();
+    request.open("POST", "https://hsh.blnq.dev/javascript-basics/form-fetch.php");
+    request.onreadystatechange = function (){
+        if (request.readyState == XMLHttpRequest.DONE){
+            console.log(request.responseText);
+        }
+    }
+    request.send(formdata);
+});
--- Gruppenarbeit/js/menu.js
+++ Gruppenarbeit/js/menu.js
@@ -3,3 +3,10 @@
  * Toggle the class "hidden" from the element with the id "menu", when the user clicks on the element with the id "menu-btn"
  * 
  */
+
+document.getElementById("menu-btn").addEventListener("click", menuOpen);
+
+function menuOpen(){
+    document.getElementById("menu").classList.toggle("hidden"); // add, remove
+    
+}
